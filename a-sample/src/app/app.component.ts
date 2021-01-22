0673c8ede380baadac4dc2d8e7fde95a9c01ad26
Alexei <alexei3333@gmail.com>
2021-01-22 18:51:22 +0300

    step

--- a-sample/src/app/app.component.ts
+++ a-sample/src/app/app.component.ts
@@ -6,12 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  
-
   title = 'a-sample';
+  outputText = "press the button to start parsing....";
+  url = "https://ib.psbank.ru/store/products/consumer-loan";
+  isLoading=false;
+  constructor()
+  {
+
+  }
+  private  appendLogText(txt:string):void
+  {
+      
+  }
+
+
   parsePage()
   {
-    alert("working");
+    //alert("working");
+    this.outputText=`start parsing ${this.url}....`;
+    this.isLoading=true;
   }
+  breakParsing()
+  {
+    this.isLoading=false;
+  }
+  
 }
