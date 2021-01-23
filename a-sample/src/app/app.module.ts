d18183158628e3819e9dd7c788c38fae37a304d5
Alexei <alexei3333@gmail.com>
2021-01-23 18:36:48 +0300

    rter

--- a-sample/src/app/app.module.ts
+++ a-sample/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { ParseformComponent } from './components/parseform/parseform.component';
 import { SecondformComponent } from './components/secondform/secondform.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavMenuComponent } from './components/_layout/nav-menu/nav-menu.component';
+import { FooterComponent } from './components/_layout/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ParseformComponent,
     SecondformComponent,
-    HomeComponent
+    HomeComponent,
+    NavMenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
