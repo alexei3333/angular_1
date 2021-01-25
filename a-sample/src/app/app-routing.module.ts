b41bb5cb05b9c42991745473da2c73442f7d6209
Alexei <alexei3333@gmail.com>
2021-01-25 21:03:56 +0300

    content child

--- a-sample/src/app/app-routing.module.ts
+++ a-sample/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './components/home/home.component';
 import { ParseformComponent } from './components/parseform/parseform.component';
 import { SecondformComponent } from './components/secondform/secondform.component';
 import { AutoformComponent } from './components/autoform/autoform.component';
+import { ContainerHolderComponent } from './components/container-holder/container-holder.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'parseform', component: ParseformComponent },
   { path: 'secondform', component: SecondformComponent },
   { path: 'autoform', component: AutoformComponent },
+  { path: 'containers', component: ContainerHolderComponent },
   { path: '**', redirectTo: '' },
 ];
 
--- a-sample/src/app/components/sub/child-one-container/child-one-container.component.ts
+++ a-sample/src/app/components/sub/child-one-container/child-one-container.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ContentChild, OnInit } from '@angular/core';
+import { ChildOneComponent } from '../child-one/child-one.component';
 
 @Component({
   selector: 'app-child-one-container',
   templateUrl: './child-one-container.component.html',
-  styleUrls: ['./child-one-container.component.scss']
+  styleUrls: ['./child-one-container.component.scss'],
 })
 export class ChildOneContainerComponent implements OnInit {
+  @ContentChild(ChildOneComponent)
+  child: ChildOneComponent | null = null;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  // get hasChild()
+  // {
 
+  // }
 }
