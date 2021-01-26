110641c4dfdd75d63a5aab0bb1725dda6029ff0d
Alexei <alexei3333@gmail.com>
2021-01-26 12:55:21 +0300

    progr

--- a-sample/src/app/services/ajax-list.service.ts
+++ a-sample/src/app/services/ajax-list.service.ts
@@ -2,12 +2,14 @@ import { Inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.prod';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
+import { apiVitrinaLw } from '../helpers/constants';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AjaxListService {
   baseUrl: string;
+  apiVitrinaLw = apiVitrinaLw;
   constructor() {
     this.baseUrl = environment.API_BASE_URL;
   }
